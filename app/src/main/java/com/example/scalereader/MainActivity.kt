5cfc588b8d5e0806c399edf2f3ef3eb2e30ecbf4
pacew-webhook <alamremix@gmail.com>
2026-08-08 00:34:43 +0900

    Update MainActivity.kt

--- app/src/main/java/com/example/scalereader/MainActivity.kt
+++ app/src/main/java/com/example/scalereader/MainActivity.kt
@@ -21,9 +21,8 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        // Memeriksa izin kamera saat aplikasi pertama kali dibuka
         if (allPermissionsGranted()) {
-            initCameraAfterLayoutReady()
+            startCamera()
         } else {
             ActivityCompat.requestPermissions(
                 this,
@@ -47,7 +46,7 @@ class MainActivity : AppCompatActivity() {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults)
         if (requestCode == CAMERA_REQUEST_CODE) {
             if (allPermissionsGranted()) {
-                initCameraAfterLayoutReady()
+                startCamera()
             } else {
                 Toast.makeText(this, "Izin kamera ditolak.", Toast.LENGTH_LONG).show()
                 finish()
@@ -55,43 +54,34 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    // Mengamankan inisialisasi agar PreviewView sudah siap secara UI
-    private fun initCameraAfterLayoutReady() {
-        val previewView = findViewById<androidx.camera.view.PreviewView>(R.id.previewView)
-        previewView.post {
-            startCamera()
-        }
-    }
-
     private fun startCamera() {
         val cameraProviderFuture = ProcessCameraProvider.getInstance(this)
 
         cameraProviderFuture.addListener({
-            try {
-                val cameraProvider = cameraProviderFuture.get()
+            val cameraProvider = cameraProviderFuture.get()
 
-                val imageAnalyzer = ScaleImageAnalyzer(
-                    context = this,
-                    onWeightDetected = { weight -> 
-                        android.util.Log.d("ScaleReader", "Berat terdeteksi: $weight")
-                    },
-                    onStatusUpdate = { message ->
-                        runOnUiThread {
-                            Toast.makeText(this, message, Toast.LENGTH_LONG).show()
-                        }
+            val imageAnalyzer = ScaleImageAnalyzer(
+                context = this,
+                onWeightDetected = { weight -> 
+                    android.util.Log.d("ScaleReader", "Berat terdeteksi: $weight")
+                },
+                onStatusUpdate = { message ->
+                    runOnUiThread {
+                        Toast.makeText(this, message, Toast.LENGTH_LONG).show()
                     }
-                )
-
-                val analysisUseCase = ImageAnalysis.Builder()
-                    .setBackpressureStrategy(ImageAnalysis.STRATEGY_KEEP_ONLY_LATEST)
-                    .build()
-                    .also { it.setAnalyzer(Executors.newSingleThreadExecutor(), imageAnalyzer) }
-
-                val previewView = findViewById<androidx.camera.view.PreviewView>(R.id.previewView)
-                val preview = Preview.Builder().build().also {
-                    it.setSurfaceProvider(previewView.surfaceProvider)
                 }
+            )
 
+            val analysisUseCase = ImageAnalysis.Builder()
+                .setBackpressureStrategy(ImageAnalysis.STRATEGY_KEEP_ONLY_LATEST)
+                .build()
+                .also { it.setAnalyzer(Executors.newSingleThreadExecutor(), imageAnalyzer) }
+
+            val preview = Preview.Builder().build().also {
+                it.setSurfaceProvider(findViewById<androidx.camera.view.PreviewView>(R.id.previewView).surfaceProvider)
+            }
+
+            try {
                 cameraProvider.unbindAll()
                 cameraProvider.bindToLifecycle(
                     this, 
@@ -101,9 +91,6 @@ class MainActivity : AppCompatActivity() {
                 )
             } catch (e: Exception) {
                 e.printStackTrace()
-                runOnUiThread {
-                    Toast.makeText(this, "Gagal memulai kamera: ${e.message}", Toast.LENGTH_LONG).show()
-                }
             }
         }, ContextCompat.getMainExecutor(this))
     }
